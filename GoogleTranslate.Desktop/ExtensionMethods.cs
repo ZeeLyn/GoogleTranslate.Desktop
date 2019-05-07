9aef3e7291f3121bedc8a2e76a81ef26129416a2
Owen <315872858@qq.com>
2019-05-07 17:28:41 +0800

    Initial commit

---
+++ GoogleTranslate.Desktop/ExtensionMethods.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleTranslate.Desktop
+{
+    public static class ExtensionMethods
+    {
+        //实现js的charAt方法
+        public static char charAt(this object obj, int index)
+        {
+            char[] chars = obj.ToString().ToCharArray();
+            return chars[index];
+        }
+        //实现js的charCodeAt方法
+        public static int charCodeAt(this object obj, int index)
+        {
+            char[] chars = obj.ToString().ToCharArray();
+            return (int)chars[index];
+        }
+
+        //实现js的Number方法
+        public static int Number(object cc)
+        {
+            try
+            {
+                long a = Convert.ToInt64(cc.ToString());
+                int b = a > 2147483647 ? (int)(a - 4294967296) : a < -2147483647 ? (int)(a + 4294967296) : (int)a;
+                return b;
+            }
+            catch (Exception)
+            {
+                return 0;
+            }
+        }
+    }
+}
---
+++ GoogleTranslate.Desktop/TranslateModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using GoogleTranslate.Desktop.Annotations;
+
+namespace GoogleTranslate.Desktop
+{
+    public class TranslateModel : INotifyPropertyChanged
+    {
+        private string _translateResult;
+
+        public string TranslateResult
+        {
+            get => _translateResult;
+            set
+            {
+                _translateResult = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
