05148cbcd8f76a34e8c7f3cfe082180a31dca62c
Owen <315872858@qq.com>
2019-05-14 16:33:10 +0800

    V1.0.4

--- src/GoogleTranslate.Desktop/App.xaml.cs
+++ src/GoogleTranslate.Desktop/App.xaml.cs
@@ -1,26 +1,46 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Windows;
 using MahApps.Metro;
+using Microsoft.Shell;
 
 namespace GoogleTranslate.Desktop
 {
     /// <summary>
     /// App.xaml 的交互逻辑
     /// </summary>
-    public partial class App : Application
+    public partial class App : Application, ISingleInstanceApp
     {
+        private const string Unique = "3BF17EF9-344B-4880-AB2C-8ECDB7860485";
+
+        [STAThread]
+        public static void Main()
+        {
+            if (!SingleInstance<App>.InitializeAsFirstInstance(Unique))
+            {
+                MessageBox.Show("已运行了一个示例，使用快捷键 Control+` 唤起.", "提示");
+                return;
+            }
+
+            var application = new App();
+            application.InitializeComponent();
+            application.Run();
+            SingleInstance<App>.Cleanup();
+        }
+
         protected override void OnStartup(StartupEventArgs e)
         {
             ThemeManager.AddAccent("Dark", new Uri("pack://application:,,,/GoogleTranslate.Desktop;component/CustomThemes.xaml"));
-
-            // get the current app style (theme and accent) from the application
-            Tuple<AppTheme, Accent> theme = ThemeManager.DetectAppStyle(Application.Current);
-
-            // now change app style to the custom accent and current theme
+            var theme = ThemeManager.DetectAppStyle(Application.Current);
             ThemeManager.ChangeAppStyle(Application.Current,
                 ThemeManager.GetAccent("Dark"),
                 theme.Item1);
             base.OnStartup(e);
+        }
+
+        public bool SignalExternalCommandLineArgs(IList<string> args)
+        {
+            return true;
         }
     }
 }
